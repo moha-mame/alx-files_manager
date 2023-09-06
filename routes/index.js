fe3395641825c4cee2a9526e258883ccce5fdd58
moha-mame <moha.mame04@gmail.com>
2023-09-06 07:27:45 -0700

    alx-files_manager

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,13 @@ const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+
+router.post('/users', UsersController.postNew);
 router.post('/files', FilesController.postUpload);
 
 module.exports = router;
